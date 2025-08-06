30b59d64f66f62bfe4bda498e1532edf1fd05a36
APYG-R <attapinya.r@ku.th>
2025-08-06 22:36:36 +0700

    Delete Lib/Discount directory

--- Lib/Discount/DefaultPricingStrategy.java
+++
@@ -1,9 +0,0 @@
-package Lib.Discount;
-import Lib.*;
-
-public class DefaultPricingStrategy implements DiscountStrategy {
-
-    public double calculatePrice(CartItem item) {
-        return item.getProduct().getPrice() * item.getQuantity();
-    }
-}
